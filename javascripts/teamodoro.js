5ffbed9da1841d61c12a00a33239be8c5d7db792
Alexis Bernard <alexis@bernard.io>
2015-04-11 19:38:18 +0200

    Update clock every 5 seconds.

--- javascripts/teamodoro.js
+++ javascripts/teamodoro.js
@@ -1,9 +1,20 @@
 Teamodoro = {
+  timeDifference: 0,
+
   start: function() {
-    SVG("canvas").clock("100%").start();
+    this.clock = SVG("canvas").clock("100%");
+    setInterval(this.updateClock.bind(this), 5000);
+  },
+
+  updateClock: function() {
+    this.clock.setMinutes(this.getMinutes());
   },
 
   timeCallback: function(response) {
     this.timeDifference = new Date - new Date(response.datetime);
+  },
+
+  getMinutes: function() {
+    return new Date(new Date() + this.timeDifference).getMinutes();
   }
 }
